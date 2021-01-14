21fa91f4ef9a61c26b03cbc87e6f25dacb7a2425
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-01-14 18:01:07 +0100

    finito da migliorare la parte estetica

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
 $('.click').click(function(){
     $(this).find('.box-list').toggle('.active')
 })
+$('p').hover(function(){
+    $(this).css("color", "black");
+},  function(){
+    $(this).css("color", "silver");
+})
+
 
 
 
